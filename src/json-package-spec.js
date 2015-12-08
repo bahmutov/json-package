d38672fc6c6ba94e1403aa9a1bbbae01e8ae02b1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-08 14:57:42 -0500

    chore(test): added simple unit test

---
+++ src/json-package-spec.js
@@ -0,0 +1,20 @@
+const la = require('lazy-ass')
+const check = require('check-more-types')
+
+/*
+require = require('really-need')
+require('./package.json', {
+  post: () => {
+    return {
+      name: 'fake',
+      version: '1.2.3'
+    }
+  }
+})
+*/
+
+/* global describe, it */
+describe('jso', () => {
+  const jso = require('./json-package')
+  it('is a function', () => la(check.fn(jso)))
+})
---
+++ src/json-package.js
@@ -0,0 +1,5 @@
+function jsonPackage (prefix) {
+
+}
+
+module.exports = jsonPackage
