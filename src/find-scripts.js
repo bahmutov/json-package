c72b2228022defeed7bdd15562681c1be466d682
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-10 17:13:27 -0500

    feat: add JSDoc

--- src/find-scripts.js
+++ src/find-scripts.js
@@ -3,6 +3,18 @@ const la = require('lazy-ass')
 const is = require('check-more-types')
 const { isFuzzyMatch, findFuzzyMatches, matchesExactly, startsWith } = require('./utils')
 
+/**
+ * Returns matching scripts keys. The match is done by prefix or
+ * by prefix in each word. When dealing with several words, you can
+ * separate using ":" or "-" characters
+ *
+ * @example findScripts('t', { test: 'mocha' }) // ["test"]
+ * @example findScripts('t:r', { test: 'mocha', 'test:record': '...' }) // ["test:record"]
+ * @example findScripts('t-r', { test: 'mocha', 'test:record': '...' }) // ["test:record"]
+ * @param {string} prefix Prefix to search for
+ * @param {object} scripts Scripts object from the package.json
+ * @returns string[]
+ */
 function findScripts (prefix, scripts) {
   la(is.unemptyString(prefix), 'expected unempty string prefix', prefix)
   la(is.object(scripts), 'expected an object of scripts', scripts)
