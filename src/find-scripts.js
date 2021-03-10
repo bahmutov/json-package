997b9188e3ae9ff0ebf1a05ab06e1b4c322de19a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-10 13:21:23 -0500

    feat: make check-more-types a prod dependency

--- src/find-scripts.js
+++ src/find-scripts.js
@@ -1,4 +1,7 @@
-function startsWith (prefix, str) {
+const la = require('lazy-ass')
+const is = require('check-more-types')
+
+function startsWith(prefix, str) {
   console.assert(typeof str === 'string', 'expected string', str)
   return str.indexOf(prefix) === 0
 }
@@ -12,7 +15,9 @@ function matchesExactly (prefix, str) {
     sameLength(prefix, str)
 }
 
-function findScripts (prefix, scripts) {
+function findScripts(prefix, scripts) {
+  la(is.unemptyString(prefix), 'expected unempty string prefix', prefix)
+
   const labels = Object.keys(scripts)
   const matchesExactlyPrefix = matchesExactly.bind(null, prefix)
   const exactMatches = labels.filter(matchesExactlyPrefix)
